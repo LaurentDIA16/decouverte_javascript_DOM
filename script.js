ed5e964154ba1868744e461d76e9ed108aba3293
LaurentDIA16 <dia16@polenumerique.re>
2022-09-02 21:19:36 +0400

    ALG02 - Voyelles et consonnes 3e version

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // ALGO 02 - VOYELLES ET CONSONNES
-// Méthode en utilisant une liste de voyelles et de consonnes avec une méthode string.includes()
-let elemButtonsix = document.getElementsByTagName("button");
-let button6 = elemButtonsix[5];
-button6.addEventListener("click",analyser);
+/** Méthode en utilisant avec une entré de valeur depuis un champ texte (input) du HTML
+    Utilisation d'une liste de voyelles et de consonnes avec une méthode string.includes()
+*/
 
-    function analyser() {
+//Entrée de la valeur dans le champ texte et déclenchement de la fonction en appuyant Enter
+const alg02 = document.getElementById('ALG02');
+alg02.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        analyser(event);
+    }
+});
+
+    function analyser(event) {
+        const mot = event.target.value
         let voyelle = "aeiouy";
         let consonne = "bcdfghjklmnpqrstvwxz";
-        let mot = "alain est la";
         let lettre;
         let nbV = 0;
         let pos;
@@ -27,6 +34,35 @@ button6.addEventListener("click",analyser);
     }
 
 
+/*
+    // ALGO 02 - VOYELLES ET CONSONNES
+    // Méthode en utilisant une liste de voyelles et de consonnes avec une méthode string.includes()
+    let elemButtonsix = document.getElementsByTagName("button");
+    let button6 = elemButtonsix[5];
+    button6.addEventListener("click",analyser);
+
+        function analyser() {
+            let voyelle = "aeiouy";
+            let consonne = "bcdfghjklmnpqrstvwxz";
+            let mot = "alain est la";
+            let lettre;
+            let nbV = 0;
+            let pos;
+            let nbC = 0;
+
+            for (pos = 0; pos < mot.length; pos++){
+                lettre = mot[pos];
+                if (voyelle.includes(lettre)) {
+                    nbV += 1;
+                    console.log("voyelle " + lettre + " en position " + pos);
+                } else if (consonne.includes(lettre)) {
+                    nbC += 1;
+                }
+            }
+            console.log("texte de "+ mot.length + " caractères");
+            console.log(nbV + " voyelles et " + nbC + " consonnes");
+        }
+-----------------------------------------------------------------------------------------------------------
 
 
     /*
@@ -57,7 +93,7 @@ button6.addEventListener("click",analyser);
         }
 
 
-
+-----------------------------------------------------------------------------------------------------------------
     
     
     /*
