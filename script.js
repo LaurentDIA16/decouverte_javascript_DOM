ffba10667062998870389888b5287fecdf544f65
LaurentDIA16 <dia16@polenumerique.re>
2022-09-02 19:19:46 +0400

    ALG02 - Voyelles et consonnes 1ère version

--- script.js
+++ script.js
@@ -1,100 +1,154 @@
-// Création de la Function recupereUL en actionnant le bouton UN
-let elemButton = document.getElementsByTagName("button");
-let button1 = elemButton[0];
-button1.addEventListener("click",recupereUL);
+// ALGO 02 - VOYELLES ET CONSONNES
+// Méthode la plus nul avec les voyelles détaillé une a une
+let elemButtonsix = document.getElementsByTagName("button");
+let button6 = elemButtonsix[5];
+button6.addEventListener("click",analyser);
 
-function recupereUL(){
-    // le DOM va chercher la 2e balise ul du fichier HTML et donner les textes associés
-    let text = document.getElementsByTagName("ul")[1].innerHTML;
-    console.log(text)
-    alert("Voici les animaux de la liste: \n "+ text);
+    function analyser() {
+        let mot = "alain est la";
+        let lettre;
+        let nbV = 0;
+        let pos;
+        let nbC = 0;
 
-    // on va aller mettre la liste d'animaux à l'endroit de l'ID "nouvelleListe"
-    document.getElementById("nouvelleListe").innerHTML = text;
-}
-
-// Création de la Function recupereUL en actionnant le bouton DEUX et querySelector
-let elemButton2 = document.getElementsByTagName("button");
-let button22 = elemButtonbis[1];
-button2.addEventListener("click",recupereUL2);
-
-function recupereUL2(){
-
-
-}
-
-
-//----------------------------------------------------------------------
-//--------------------------------------------------------------------------------------
-// Création de la Function recupereUL en actionnant le bouton DEUX et querySelector
-let elemButtonbis = document.getElementsByTagName("button");
-let button2 = elemButtonbis[1];
-button1.addEventListener("click",recupereUL2);
-console.log(button2);
-
-function recupereUL2(){
-    // Utilisation de querySelectorAll
-    const nodeList= document.querySelectorAll("li");
-    for (let i = 0; i<nodeList.length;i++){
-    nodeList[i].style.backgroundColor = 'pink'
-    }
-};
-
-
-//--------------------------------------------------------------------------------------
-//Serie 1 exo ALGO - ALG00 - nomPropre
-let elemButtonTer = document.getElementsByTagName("button");
-let button3 = elemButtonTer[2];
-button3.addEventListener("click",nomPropre);
-
-function nomPropre() {
-    let mot;
-    let reste;
-    let maj;
-    mot=prompt("Entrez un nom:");
-    mot = mot.toLowerCase();
-    maj = mot.charAt(0);
-    maj = maj.toUpperCase();
-    reste = mot.substring(1);
-    return mot = alert(maj.concat(reste));
-}
-
-//--------------------------------------------------------------------------------------
-//Serie 1 exo ALGO - ALG01 - Epeler avec méthode SPLIT()
-
-let elemButtonQua = document.getElementsByTagName("button");
-let button4 = elemButtonQua[3];
-button4.addEventListener("click",EpelerSplit);
-
-function EpelerSplit(motAEpeler){
-    motAEpeler=prompt("Entrez un mot ou une expression:");
-    let maj = motAEpeler.toUpperCase(); //mettre variable en majuscule
-    let array = maj.split(""); //séparé es caractères avec une virgule et les mettres dans une table
-    alert(array);
-    document.getElementById("motEpelerSplit").innerHTML = array;
-}
-
-//-------------------------------------------------------------------------------------------
-// Serie 1 exo ALGO - ALG01 - Epeler avec boucle FOR
-
-let elemButtonCin = document.getElementsByTagName("button");
-let button5 = elemButtonCin[4];
-button5.addEventListener("click",EpelerFor);
-
-function EpelerFor(motAEpeler,){
-    let motEpele =""; //variable qui va stocker le mot final
-    motAEpeler=prompt("Entrez un mot ou une expression:");
-    let maj = motAEpeler.toUpperCase(); //Mettre la variable en majuscule
-
-    //Boucle FOR sur chaque caractères de la variable motAEpeler
-        for (let i = 0;i < maj.length;i++){
-            let n = maj[i];
-            motEpele = motEpele + (n + ","); //Après chaque caractères on met une virgule
+        for (pos = 0; pos < mot.length; pos++){
+            lettre = mot[pos];
+            if (lettre == "a" || lettre == "e" || lettre == "i" || lettre == "o"|| lettre == "o" || lettre == "y") {
+                nbV += 1;
+                console.log("voyelle " + lettre + " en position " + pos);
+            } else {
+                nbC += 1;
+            }
         }
-        motEpele = motEpele.substring(0,motEpele.length-1); //On enlève le dernier caratère de la chaine de caractère
-        document.getElementById("motEpelerFor").innerHTML = motEpele; //On va insérer dans la page HTML la variable motEpele dans la div motEpeler
+        console.log("texte de "+ mot.length + " caractères");
+        console.log(nbV + " voyelles et " + (nbC-2) + " consonnes");
     }
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //let voyelle = "aeiouy";
+    //let consonne = "bcdfghjklmnpqrstvwxz";
+    
+    
+    /*
+    // Création de la Function recupereUL en actionnant le bouton UN
+    let elemButton = document.getElementsByTagName("button");
+    let button1 = elemButton[0];
+    button1.addEventListener("click",recupereUL);
+
+    function recupereUL(){
+        // le DOM va chercher la 2e balise ul du fichier HTML et donner les textes associés
+        let text = document.getElementsByTagName("ul")[1].innerHTML;
+        console.log(text)
+        alert("Voici les animaux de la liste: \n "+ text);
+
+        // on va aller mettre la liste d'animaux à l'endroit de l'ID "nouvelleListe"
+        document.getElementById("nouvelleListe").innerHTML = text;
+    }
+
+    // Création de la Function recupereUL en actionnant le bouton DEUX et querySelector
+    let elemButton2 = document.getElementsByTagName("button");
+    let button22 = elemButtonbis[1];
+    button2.addEventListener("click",recupereUL2);
+
+    function recupereUL2(){
+
+
+    }
+
+
+    //----------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------
+    // Création de la Function recupereUL en actionnant le bouton DEUX et querySelector
+    let elemButtonbis = document.getElementsByTagName("button");
+    let button2 = elemButtonbis[1];
+    button1.addEventListener("click",recupereUL2);
+    console.log(button2);
+
+    function recupereUL2(){
+        // Utilisation de querySelectorAll
+        const nodeList= document.querySelectorAll("li");
+        for (let i = 0; i<nodeList.length;i++){
+        nodeList[i].style.backgroundColor = 'pink'
+        }
+    };
+
+
+    //--------------------------------------------------------------------------------------
+    //Serie 1 exo ALGO - ALG00 - nomPropre
+    let elemButtonTer = document.getElementsByTagName("button");
+    let button3 = elemButtonTer[2];
+    button3.addEventListener("click",nomPropre);
+
+    function nomPropre() {
+        let mot;
+        let reste;
+        let maj;
+        mot=prompt("Entrez un nom:");
+        mot = mot.toLowerCase();
+        maj = mot.charAt(0);
+        maj = maj.toUpperCase();
+        reste = mot.substring(1);
+        return mot = alert(maj.concat(reste));
+    }
+
+    //--------------------------------------------------------------------------------------
+    //Serie 1 exo ALGO - ALG01 - Epeler avec méthode SPLIT()
+
+    let elemButtonQua = document.getElementsByTagName("button");
+    let button4 = elemButtonQua[3];
+    button4.addEventListener("click",EpelerSplit);
+
+    function EpelerSplit(motAEpeler){
+        motAEpeler=prompt("Entrez un mot ou une expression:");
+        let maj = motAEpeler.toUpperCase(); //mettre variable en majuscule
+        let array = maj.split(""); //séparé es caractères avec une virgule et les mettres dans une table
+        alert(array);
+        document.getElementById("motEpelerSplit").innerHTML = array;
+    }
+
+    //-------------------------------------------------------------------------------------------
+    // Serie 1 exo ALGO - ALG01 - Epeler avec boucle FOR
+
+    let elemButtonCin = document.getElementsByTagName("button");
+    let button5 = elemButtonCin[4];
+    button5.addEventListener("click",EpelerFor);
+
+    function EpelerFor(motAEpeler,){
+        let motEpele =""; //variable qui va stocker le mot final
+        motAEpeler=prompt("Entrez un mot ou une expression:");
+        let maj = motAEpeler.toUpperCase(); //Mettre la variable en majuscule
+
+        //Boucle FOR sur chaque caractères de la variable motAEpeler
+            for (let i = 0;i < maj.length;i++){
+                let n = maj[i];
+                motEpele = motEpele + (n + ","); //Après chaque caractères on met une virgule
+            }
+            motEpele = motEpele.substring(0,motEpele.length-1); //On enlève le dernier caratère de la chaine de caractère
+            document.getElementById("motEpelerFor").innerHTML = motEpele; //On va insérer dans la page HTML la variable motEpele dans la div motEpeler
+        }
+    
 //--------------------------------------------------------------------------------
 
 /** frigo pkst 9 10  evenementiel (old skill)
